56cac92f0b85122864ee347ea996c5b94717751e
pringshia <pratik@ringshia.com>
2022-12-07 01:28:01 -0500

    Add day7

---
+++ day7/README.md
@@ -0,0 +1,4 @@
+# Learnings
+
+- `numericalArray.sort()` doesn't really work as expected.
+  - I needed to to `numericalArray.sort((a, b) => a - b)`
---
+++ day7/p1.ts
@@ -0,0 +1,60 @@
+const input = await Deno.readTextFile("input.txt");
+
+const lines = input.split("\n").filter(l => l !== "");
+
+function discoverFS(lines) {
+  let rootFS = {"/": {} };
+  let currPath = ["/"];
+
+  for (let line of lines) {
+    if (line.startsWith("$ ")) {
+      // is input
+      let input = line.replace("$ ", "").trim();
+      let [cmd, ...args] = input.split(" ");
+      if (cmd === "cd") {
+        if (args[0] === "/") currPath = ["/"];
+        else if (args[0] === "..") currPath.pop();
+        else currPath.push(args[0]);
+      }
+      else if (cmd === "ls") {
+        continue;
+      }
+    } else {
+      // is output for ls
+      let [info, name] = line.split(" ");
+      let ref = rootFS;
+      for (let subpath of currPath) {
+        if (!(subpath in ref)) ref[subpath] = {};
+        ref = ref[subpath];
+      }
+      if (info === "dir") {
+        if (!ref[name])
+          ref[name] = {};
+      } else {
+        ref[name] = info;
+      }
+    }
+  }
+  return rootFS;
+}
+
+function getFolderSize(fname, folder, journal) {
+  return Object.entries(folder).map(([name, value]) => {
+    if (typeof value === "object") {
+      let fullPath = fname+name+"/";
+      const size = getFolderSize(fullPath, value, journal);
+      // journal
+      journal[fullPath] = size;  
+      return size;
+    } else return parseInt(value, 10);
+  }).reduce((partial, v) => partial + v, 0);
+}
+
+const fs = discoverFS(lines);
+let journal = {};
+const dirSizes = getFolderSize("/", fs["/"], journal);
+
+const result = Object.values(journal).filter(val => val <= 100000).reduce((partial, val) => partial + val, 0);
+// console.log(fs, journal);
+
+console.log("RESULT:", result);
